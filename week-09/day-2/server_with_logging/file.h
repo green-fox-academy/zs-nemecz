79ba7248d81ad6f7fbc4695a4258b4101b732d56
Zsuzsanna Nemecz <zsuzska.nemecz@gmail.com>
2017-06-28 10:59:54 +0200

    internet server with logging to .txt file

---
+++ week-09/day-2/server_with_logging/file.h
@@ -0,0 +1,15 @@
+#ifndef FILE_H_INCLUDED
+#define FILE_H_INCLUDED
+#include <time.h>
+
+#define MAX_FILENAME_LEN    1024
+#define STRFTIME_BUFF_LEN   100
+#define READ_BUFFER_LEN     1024
+
+char write_file_name[MAX_FILENAME_LEN];
+
+int get_file_name(char *file_name);
+int get_write_file_name();
+int write_data_to_file(char *data);
+
+#endif // FILE_H_INCLUDED
